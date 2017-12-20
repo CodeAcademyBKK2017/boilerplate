b240e841453c2c152e01c5b093f3c908ab198705
Benjapol Suksakulchai <assassin@Benjapols-iMac.local>
2017-12-20 16:36:39 +0700

    Update support platform ios & android keyboardAvoiding

--- app/index.js
+++ app/index.js
@@ -9,22 +9,27 @@ import Footer from './components/Footer/Footer.component';
 import React, {Component} from 'react';
 import styles from './index.style';
 import Title from './components/Title/Title.component';
-import {View} from 'react-native';
+import {KeyboardAvoidingView, Platform, View} from 'react-native';
 
 export default class App extends Component {
   state = {
     content: ''
   }
+  WrapperView = Platform.select(
+    {ios: KeyboardAvoidingView,
+      android: View
+    }
+  )
   changeText  = (text) => {
     this.setState({content: text});
   }
   render () {
     return (
-      <View style={styles.container} >
+      <this.WrapperView style={styles.container} behavior={'padding'} >
         <Title/>
         <Content  onTextChange={this.changeText} />
         <Footer characterCount={this.state.content.length}/>
-      </View>
+      </this.WrapperView>
     );
   }
 }
