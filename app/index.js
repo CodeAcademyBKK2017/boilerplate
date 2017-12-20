655bb790937dfae162852b2b5491a60b3b1779b9
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-12-20 16:22:29 +0700

    use KeyboardAvoidingView with wrapper

--- app/index.js
+++ app/index.js
@@ -10,13 +10,18 @@ import React, {Component} from 'react';
 import styles from './index.style';
 import Title from './components/Title/Title.component';
 import {
-  View
+  KeyboardAvoidingView, Platform, View
 } from 'react-native';
 
 export default class App extends Component {
   state = {
     textContent: ''
   }
+
+  WrapperView = Platform.select({
+    ios: KeyboardAvoidingView,
+    android: View
+  });
 
   onChangeTextContent = (text) => {
     this.setState({
@@ -26,11 +31,12 @@ export default class App extends Component {
 
   render () {
     return (
-      <View style={styles.container}>
+      <this.WrapperView style={[styles.container]}
+        behavior='padding'>
         <Title/>
         <Content style={styles.fill} onChangeTextContent={this.onChangeTextContent}/>
         <Footer textContent={this.state.textContent}/>
-      </View>
+      </this.WrapperView>
     );
   }
 }
