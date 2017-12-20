5df0cd0ada8e6e2a32bff983064806b52dcca820
pheerah <pheerah.p@gmail.com>
2017-12-20 12:02:51 +0700

    UI complete

--- app/index.style.js
+++ app/index.style.js
@@ -1,14 +1,14 @@
 import {
   StyleSheet
 } from 'react-native';
-  
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#F5FCFF',
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    alignItems: 'center'
   }
 });
-  
+
 export default styles;
