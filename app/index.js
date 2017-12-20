b0cb6792f3784ba813cd29092878cc1fc6b1384c
nanny <nanny@nannys-iMac.local>
2017-12-20 10:59:01 +0700

    AppReact

--- app/index.js
+++ app/index.js
@@ -6,31 +6,13 @@
 
 import React, {Component} from 'react';
 import {
-  Platform,
   StyleSheet,
-  Text,
-  View,
-  TextInput,
-  style,
-  Button,
-  Modal,
-  TouchableOpacity,
-  TouchableWithoutFeedback
+  View
 } from 'react-native';
 import TextAlign from './components/TextArea/TextArea.components';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
 
 export default class App extends Component {
-  getName = () => 'Yo'
-  state = {
-    text: '',
-  };
   render () {
     return (
       <View style={styles.container}>
@@ -48,34 +30,5 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'rgba(37, 8, 10, 0.78)',
-    alignItems: 'center',
-  },
-  innerContainer: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    width:'80%',
-    height:200,
-    justifyContent: 'center'
-    
-  },
-  bt:{
-    backgroundColor:'yellow',
-    padding:10
-    
-    }
+  }
 });
