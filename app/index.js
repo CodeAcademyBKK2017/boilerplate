168a59f1efa5d3faa2b12602bd41b4ad96fd6b58
Rung <rung@Rungs-iMac.local>
2017-12-20 11:03:28 +0700

    wed 11:03

--- app/index.js
+++ app/index.js
@@ -5,32 +5,22 @@
  */
 
 import React, {Component} from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  TextInput
-} from 'react-native';
 import TextArea from './components/TextArea/TextArea.component.js';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
-
-export default class App extends Component<{}> {
+export default class App extends Component {
   state = {
-    text: '',
+    text: ''
   }
   getName = () => 'Yo'
 
   render () {
     return (
       <View style={styles.container}>
-         <TextArea /> 
+        <TextArea /> 
       </View>
     );
   }
@@ -39,18 +29,8 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:'center',
+    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#F5FCFF'
+  }
 });
