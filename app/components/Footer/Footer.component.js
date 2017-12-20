6a63d4394195e7369146c9b04353e9bf06ce2651
Benjapol Suksakulchai <assassin@Benjapols-iMac.local>
2017-12-20 15:01:59 +0700

    Update Toucable Opacity for SaveButton

--- app/components/Footer/Footer.component.js
+++ app/components/Footer/Footer.component.js
@@ -1,13 +1,15 @@
 // import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import styles from './Footer.style';
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
 
 export default class Footer extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.textBold}>Save</Text>
+        <TouchableOpacity>
+          <Text style={styles.textBold}>Save</Text>
+        </TouchableOpacity>
         <Text style={styles.text}>150 characters</Text>
       </View>
     );
