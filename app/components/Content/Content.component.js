6494cde9b351e87813b5c15a82d1d708c36ff02a
Benjapol Suksakulchai <assassin@Benjapols-iMac.local>
2017-12-20 13:04:57 +0700

    Update All component pass lint

--- app/components/Content/Content.component.js
+++ app/components/Content/Content.component.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import styles from './Content.style';
 import {Text, TextInput, View} from 'react-native';
--- app/components/Footer/Footer.component.js
+++ app/components/Footer/Footer.component.js
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import styles from './Footer.style';
 import {Text, View} from 'react-native';
