96ff591a4146bec1fa0224a944a9fe6bcd842c65
aondcon <aonspace@Jakkrits-iMac.local>
2017-12-20 10:20:03 +0700

    First commit

--- app/index.js
+++ app/index.js
@@ -6,34 +6,22 @@
 
 import React, {Component} from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu'
-});
-
-export default class App extends Component<{}> {
-  getName = () => 'Yo'
-
+export default class App extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.title}>Content</Text>
+        <TextInput
+          multiline={true}
+          placeholder='Type here!'
+          style={styles.textInput}
+        />
       </View>
     );
   }
@@ -44,16 +32,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#F5FCFF',
+    padding: 20
   },
-  welcome: {
+  title: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10
+    width: '100%'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+  textInput: {
+    width: '100%',
+    marginTop: 10,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#ccc',
+    borderRadius: 5,
+    height: 100,
+    padding: 12
   }
 });
