2226488b6492f4480577ea02528f38e64638de8e
Teeraphong Chaichalermpreecha <teeraphongchaichalermpreecha@Teeraphongs-MacBook-Air.local>
2017-12-20 10:37:33 +0700

    init

---
+++ app/components/TextArea/TextArea.component.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import styles from './TextArea.style.js';
+import {Text, TextInput, View} from 'react-native';
+
+class TextArea extends Component {
+  render () {
+    return (
+      <View>
+        <Text>Content</Text>
+        <TextInput
+          style={styles.textArea}
+          multiline = {true}
+        />
+      </View>
+    );
+  }
+}
+
+export default TextArea;
