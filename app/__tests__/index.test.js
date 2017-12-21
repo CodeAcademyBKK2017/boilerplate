512be7064a2769e2ebe3a9d71b2b90e73c4080c3
pheerah <pheerah.p@gmail.com>
2017-12-21 12:37:15 +0700

    UI Completed

--- app/__tests__/index.test.js
+++ app/__tests__/index.test.js
@@ -6,18 +6,44 @@ import renderer from 'react-test-renderer';
 import {shallow} from 'enzyme';
 
 describe('App', () => {
+  let tree, instance;
+  beforeEach(() => {
+    tree = shallow(<App/>);
+    instance = tree.instance();
+  });
   it('renders correctly', () => {
     const tree = renderer.create(
       <App />
     );
     expect(tree).toBeDefined();
   });
-  it('onTypeContent: should change the length', () => {
-    const tree = shallow(<App/>);
-    const instance = tree.instance();
+  it('onTypeContent: should change the value of content', () => {
     instance.onTypeContent('hello');
-    expect(instance.state.content.length).toBe(5);
-    instance.onTypeContent('');
-    expect(instance.state.content.length).toBe(0);
+    expect(instance.state.content).toBe('hello');
+    instance.onTypeContent('');  
+    expect(instance.state.content).toBe('');
+  });
+  it('onTypeTitle: should change the value of title', () => {
+    instance.onTypeTitle('test');
+    expect(instance.state.title).toBe('test');
+    instance.onTypeTitle('');  
+    expect(instance.state.title).toBe('');
+  });
+  it('onSavePress: should add the value of title', () => {
+    const newData = {
+      title: 'title',
+      content: 'content'
+    };
+    instance.setState(newData);
+    instance.onSavePress();
+    const expectState = {
+      title: '',
+      content: '',
+      notes: [{
+        title: 'title',
+        content: 'content'
+      }]
+    };
+    expect(instance.state).toMatchObject(expectState);
   });
 });
