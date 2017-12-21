b9bf97bd59e6e0eb72ff1e2e16568f511442044f
nanny <nanny@nannys-iMac.local>
2017-12-21 11:36:18 +0700

    AppReact

--- app/index.js
+++ app/index.js
@@ -33,7 +33,7 @@ export default class App extends Component {
     
     this.setState(
       {'NOTES': newNotes}, () => {
-        console.log(this.state.NOTES);
+        // console.log(this.state.NOTES);
       }
     );
   }
