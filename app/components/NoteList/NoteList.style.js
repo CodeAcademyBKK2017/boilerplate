1c2724b2a6ce0e44904c7bb4c6bd3d9fb1c8830f
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2017-12-21 15:32:59 +0700

    add style

---
+++ app/components/NoteList/NoteList.style.js
@@ -0,0 +1,26 @@
+import {
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%'
+  },
+  noteTitle: {
+    fontWeight: 'bold'
+  },
+  itemContainer: {
+    paddingVertical: 10
+  },
+  itemTitle: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    paddingBottom: 10
+  },
+  itemContent: {
+    fontSize: 12
+  }
+});
+
+export default styles;
