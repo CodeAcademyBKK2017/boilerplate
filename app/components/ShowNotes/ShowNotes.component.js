44b8fd9f4ce1b864b15058dd211b4459692b4ed2
chaowalit <dekchaowalit_123@hotmail.com>
2017-12-22 09:33:44 +0700

    update alert

--- app/components/ShowNotes/ShowNotes.component.js
+++ app/components/ShowNotes/ShowNotes.component.js
@@ -3,18 +3,24 @@ import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import ShowNotesStyle from './ShowNotes.style';
 import {
+  Alert,
   FlatList,
   Text,
+  TouchableOpacity,
   View
 } from 'react-native';
 
 export default class ShowNotes extends Component {
     _keyExtractor = (item) => item.uuid;
     _renderItem = ({item}) => 
-      <View style={ShowNotesStyle.boxShowNotes}>
-        <Text>{item.title}</Text>
-        <Text>{item.content}</Text>
-      </View>;
+      <TouchableOpacity onPress={this.onShowAlert(item.title, item.content)}>
+        <View style={ShowNotesStyle.boxShowNotes}>
+          <Text>{item.title}</Text>
+          <Text>{item.content}</Text>
+        </View>
+      </TouchableOpacity>;
+
+    onShowAlert = (title, content) => () => Alert.alert(title, content);
     
     render () {
       return (
