2bef446c0070d5e122c2d58850ff79241f8b747e
Teeraphong Chaichalermpreecha <teeraphongchaichalermpreecha@Teeraphongs-MacBook-Pro.local>
2018-01-03 14:41:00 +0700

    react-native-vector-icons

--- app/components/Content/Content.component.js
+++ app/components/Content/Content.component.js
@@ -1,3 +1,4 @@
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
 import noop from 'lodash/noop';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
@@ -12,7 +13,8 @@ class Content extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Please type your note below</Text>
+        <Text style={styles.text}>Please type your note below <Icon name='note' size={16} color='#333' /></Text>
+
         <TextInput
           style={styles.textinput}
           underlineColorAndroid='transparent'
