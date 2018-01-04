983b5983fe6744e6cb2f7d0f84bb7ff9bd783a64
Thanaphat Suwannikornkul <onsensei@hotmail.com>
2018-01-04 16:51:23 +0700

    use drawer nav

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,9 @@
 import AboutApp from '../components/About/AboutApp.component';
 import AboutDev from '../components/About/AboutDev.component';
 import App from '../index';
+import Icon from 'react-native-vector-icons/Foundation';
 import React from 'react';
-import {StackNavigator, TabNavigator} from 'react-navigation';
+import {DrawerNavigator, StackNavigator, TabNavigator} from 'react-navigation';
 
 const MyApp = TabNavigator({
   AboutApp: {
@@ -29,15 +30,44 @@ const MyApp = TabNavigator({
   // }
 });
 
-const RootNavigator = StackNavigator({
+const getRootHomeNavOpt = ({navigation}) => {
+  const onPressMenuButton = () => {
+    navigation.navigate('DrawerToggle');
+  };
+  
+  const MenuButton = ( // eslint-disable-next-line
+    <Icon.Button name='list' color='black' iconStyle={{padding: 10}} backgroundColor='transparent' onPress={onPressMenuButton}/>
+  );
+
+  return {
+    headerTitle: 'Note Taker',
+    headerLeft: MenuButton
+  };
+};
+
+const RootHome = StackNavigator({
   Home: {
     screen: App,
+    navigationOptions: getRootHomeNavOpt
+  }
+});
+
+const RootAbout = StackNavigator({
+  About: {
+    screen: MyApp,
+    navigationOptions: getRootHomeNavOpt
+  }
+});
+
+const RootNavigator = DrawerNavigator({
+  Home: {
+    screen: RootHome,
     navigationOptions: {
       headerTitle: 'Note Taker'
     }
   },
   About: {
-    screen: MyApp
+    screen: RootAbout
   }
 });
 
