3b83cb9e71b2848a28b384feec114e2777350e3e
Teeraphong Chaichalermpreecha <teeraphongchaichalermpreecha@Teeraphongs-MacBook-Pro.local>
2018-01-04 16:38:51 +0700

    add drawernavigation

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,17 @@
 import AboutRoutes from './about.route';
 import App from '../index';
-import {StackNavigator} from 'react-navigation';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+import React from 'react';
+import {DrawerNavigator, StackNavigator} from 'react-navigation';
+import {
+  Text
+} from 'react-native';
 
-const Router = StackNavigator({
+const openDrawer = (navigation) => () => {
+  navigation.navigate('DrawerOpen');
+};
+
+const DrawerStack = DrawerNavigator({
   Main: {
     screen: App,
     navigationOptions: {
@@ -13,5 +22,42 @@ const Router = StackNavigator({
     screen: AboutRoutes
   }
 });
+
+const styleHamburger = {
+  paddingLeft: 20
+};
+
+const Router = StackNavigator(
+  {
+    drawerMenu: {screen: DrawerStack}
+  },
+  {
+    headerMode: 'float',
+    navigationOptions: ({navigation}) => ({
+      headerStyle: {
+        backgroundColor: 'green'
+      },
+      title: 'Start taking notes',
+      headerTitleStyle: {
+        color: 'white'
+      },
+      headerLeft: <Text style={styleHamburger} onPress={openDrawer(navigation)}><Icon name='menu' size={22} color='#fff' /></Text>
+    })
+  }
+);
+
+// const Router = StackNavigator(
+//   {
+//     Main: {
+//       screen: App,
+//       navigationOptions: {
+//         title: 'Start taking notes'
+//       }
+//     },
+//     About: {
+//       screen: AboutRoutes
+//     }
+//   }
+// );
 
 export default Router;
