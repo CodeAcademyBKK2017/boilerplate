2d83044de1b140a4b7a93b10e13d833ca57ed479
nanny2536 <ini3nany_nan@hotmail.com>
2018-01-08 00:59:09 +0700

    mon 00:59

--- Router/index.js
+++ Router/index.js
@@ -1,21 +1,70 @@
 import About from '../app/components/About/About.component';
 import App from '../app/index';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import React from 'react';
-import {StackNavigator} from 'react-navigation';
+import {DrawerNavigator, StackNavigator} from 'react-navigation';
 
-const RootNavigator = StackNavigator({
+const openMenu = (navigation) => () => {
+  navigation.navigate('DrawerToggle');  
+};
+const iconMenu = (navigation) => () => {
+  
+};
+const MyApp = DrawerNavigator({
   Home: {
-    screen: App,
+    screen: StackNavigator({
+      Home: {
+        screen: App,
+        navigationOptions: ({navigation}) => ({
+          headerTitle: 'Home',
+          headerLeft: <Icon name='bars'
+            onPress={openMenu(navigation)}
+            style={{marginLeft: 10}} size={20} color='#000' />
+        })
+      },
+      About: {
+        screen: About,
+        navigationOptions: {
+          headerTitle: 'About'
+        }
+      }
+    }),
     navigationOptions: {
-      headerTitle: 'Home'
+      drawerLabel: 'Home',
+      initialRouteName: true
     }
   },
-  About: {
-    screen: About,
+  AboutTab: {
+    screen: StackNavigator({
+      About: {
+        screen: About,
+        navigationOptions: ({navigation}) => ({
+          headerTitle: 'About',
+          headerLeft: <Icon name='bars'
+            onPress={openMenu(navigation)}
+            style={{marginLeft: 10}} size={20} color='#000' />
+        })
+      }
+    }),
     navigationOptions: {
-      headerTitle: 'About'
+      drawerLabel: 'About',
+      initialRouteName: true
     }
   }
 });
+// const RootNavigator = StackNavigator({
+//   Home: {
+//     screen: App,
+//     navigationOptions: {
+//       headerTitle: 'Home'
+//     }
+//   },
+//   About: {
+//     screen: About,
+//     navigationOptions: {
+//       headerTitle: 'About'
+//     }
+//   }
+// });
 
-export default RootNavigator;
+export default MyApp;
