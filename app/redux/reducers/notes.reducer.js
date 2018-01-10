7e3397b2750611bd18907f70b66686df3ccedf8c
deathsky0725 <assassin@Benjapols-iMac.local>
2018-01-10 10:47:41 +0700

    Change Reducer to Note  Reducer

---
+++ app/redux/reducers/notes.reducer.js
@@ -0,0 +1,18 @@
+import {filterNote} from '../../utils/transformerutil';
+
+export default (previousState = [], action) => {
+  switch (action.type) {
+  case 'ADD_NOTE': {
+    return [...previousState, action.payload];
+  }
+  case 'DELETE_NOTE': {
+    const remainNote = filterNote(previousState, action.payload);
+    return remainNote;
+  }
+  case 'POPULATE_NOTE': {
+    return action.payload;
+  }
+  default:
+    return previousState;
+  }
+};
--- app/redux/reducers/root.reducer.js
+++ app/redux/reducers/root.reducer.js
@@ -1,21 +1,6 @@
+import notesReducer from './notes.reducer';
 import {combineReducers} from 'redux';
-import {filterNote} from '../../utils/transformerutil';
 
 export default combineReducers({
-  notes: (previousState = [], action) => {
-    switch (action.type) {
-    case 'ADD_NOTE': {
-      return [...previousState, action.payload];
-    }
-    case 'DELETE_NOTE': {
-      const remainNote = filterNote(previousState, action.payload);
-      return remainNote;
-    }
-    case 'POPULATE_NOTE': {
-      return action.payload;
-    }
-    default:
-      return previousState;
-    }
-  }
+  notes: notesReducer
 });
