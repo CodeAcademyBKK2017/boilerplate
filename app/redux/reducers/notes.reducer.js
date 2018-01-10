2404cb3c0dcc6f03a712671707101264f7117ff2
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-01-10 10:49:26 +0700

    restructure : move notes reducer to new file

---
+++ app/redux/reducers/notes.reducer.js
@@ -0,0 +1,23 @@
+import TransformerUtil from '../../utils/TransformerUtil';
+
+const notes = (prevState = [], action) => {
+  switch (action.type) {
+  case 'ADD_NOTE': {
+    return [...prevState, action.payload];
+  }
+    
+  case 'DELETE_NOTE': {
+    return TransformerUtil.removeNote(prevState, action.payload.id);
+  }
+
+  case 'POPULATE_NOTES': {
+    return action.payload;
+  }
+  
+  default: {
+    return prevState;
+  }
+  }
+};
+
+export default notes;
--- app/redux/reducers/root.reducer.js
+++ app/redux/reducers/root.reducer.js
@@ -1,25 +1,7 @@
-import TransformerUtil from '../../utils/TransformerUtil';
+import Notes from './notes.reducer';
 import {combineReducers} from 'redux';
 
 export default combineReducers({
   init: () => ({}), // reducer
-  notes: (prevState = [], action) => {
-    switch (action.type) {
-    case 'ADD_NOTE': {
-      return [...prevState, action.payload];
-    }
-      
-    case 'DELETE_NOTE': {
-      return TransformerUtil.removeNote(prevState, action.payload.id);
-    }
-
-    case 'POPULATE_NOTES': {
-      return action.payload;
-    }
-    
-    default: {
-      return prevState;
-    }
-    }
-  }
+  notes: Notes
 });
