3e6a8768d56dcc69b348a814c76df3ad97824ecb
deathsky0725 <assassin@Benjapols-iMac.local>
2018-01-10 10:58:04 +0700

    Create Actions constant for using in multiple time

--- app/redux/reducers/notes.reducer.js
+++ app/redux/reducers/notes.reducer.js
@@ -1,15 +1,16 @@
+import {ADD_NOTE, DELETE_NOTE, POPULATE_NOTE} from '../actions/index.actions';
 import {filterNote} from '../../utils/transformerutil';
 
 export default (previousState = [], action) => {
   switch (action.type) {
-  case 'ADD_NOTE': {
+  case ADD_NOTE: {
     return [...previousState, action.payload];
   }
-  case 'DELETE_NOTE': {
+  case DELETE_NOTE: {
     const remainNote = filterNote(previousState, action.payload);
     return remainNote;
   }
-  case 'POPULATE_NOTE': {
+  case POPULATE_NOTE: {
     return action.payload;
   }
   default:
