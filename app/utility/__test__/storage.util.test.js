a097c34069013241c97bc4e127b5646aabd96ecd
pheerah <pheerah.p@gmail.com>
2018-01-11 10:46:49 +0700

    test case

--- app/utility/__test__/storage.util.test.js
+++ app/utility/__test__/storage.util.test.js
@@ -1,40 +1,23 @@
 import React from 'react';
-import StorageUtil from '.././storage.util';
+import StorageUtil from '../storage.util';
 import {AsyncStorage} from 'react-native';
-import {createStore} from 'redux';
-import {shallow} from 'enzyme';
 
 jest.mock('AsyncStorage', () => ({
-  getItem: jest.fn(() => Promise.resolve('')),
-  setItem: jest.fn(() => Promise.resolve())
+  setItem: jest.fn(),
+  getItem: jest.fn(() => Promise.resolve('{}'))
 }));
 
-const store = createStore(() => ({}));
-
-describe('App', () => {
-  let appComp;
-  let appInstance;
-  beforeEach(() => {
-    appComp = <StorageUtil store={store}/>;
-    const wrapper = shallow(appComp);
-    appInstance = wrapper.find('App').shallow().instance();
-  });
-  xit('onSavePress success Async', async () => {
-    const title = 'my test title';
-    const content = 'my test content';
-    appInstance.setState({title: title, content: content});
-    await appInstance.onSavePress();
-    const expectedNote = {
-      'title': 'my test title',
-      'content': 'my test content'
-    };
-    expect(expectedNote.setItem).toBeCalled();
+describe('AboutApp', () => {
+  it('Check Function setItem', () => { // example to test class methods
+    StorageUtil.setItem('notes', {});
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith('notes', '{}');
   });
 
-  xit('onSavePress failure Async', async () => {
-    AsyncStorage.setItem.mockClear();
-    StorageUtil.addNote.mockImplementation(() => Promise.reject('API failed'));
-    await StorageUtil.onSavePress();
-    expect(AsyncStorage.setItem).not.toBeCalled();
+  it('Check Function getItem', async () => {
+    const expectedData = {};
+
+    const result = await StorageUtil.getItem('notes');
+    expect(result).toEqual(expectedData);
   });
+
 });
