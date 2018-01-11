cfd33f8b6575bcc48b17c316f42be9a60e4c00a8
deathsky0725 <assassin@Benjapols-iMac.local>
2018-01-11 16:28:02 +0700

    update middle ware

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,7 +1,7 @@
 import rootReducer from './reducers/root.reducer';
 // import someReduxMiddleware from 'some-redux-middleware';
 // import someOtherReduxMiddleware from 'some-other-redux-middleware';
-import {compose, createStore/* , applyMiddleware*/} from 'redux';
+import {applyMiddleware, compose, createStore/* , applyMiddleware*/} from 'redux';
 
 const enhancerList = [];
 const devToolsExtension = window && window.__REDUX_DEVTOOLS_EXTENSION__;
@@ -9,8 +9,13 @@ const devToolsExtension = window && window.__REDUX_DEVTOOLS_EXTENSION__;
 if (typeof devToolsExtension === 'function') {
   enhancerList.push(devToolsExtension());
 }
-
-const composedEnhancer = compose(/* applyMiddleware(someReduxMiddleware, someOtherReduxMiddleware),*/ ...enhancerList);
+const logger = ({dispatch, getState}) => (next) => (action) => {
+  // console.log('This is come from ACTION', action);
+  // if (action.type !== 'Navigation/NAVIGATE') {
+  next(action);
+  // }
+};
+const composedEnhancer = compose(applyMiddleware(logger), ...enhancerList);
 
 export const initStore = () => createStore(rootReducer, {}, composedEnhancer);
 
