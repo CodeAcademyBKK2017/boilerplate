ca178efda2cdf19d693b098aee891c8db78df04a
pheerah <pheerah.p@gmail.com>
2018-01-12 14:35:09 +0700

    test case

---
+++ app/redux/reducers/__test__/loader.reducer.test.js
@@ -0,0 +1,22 @@
+import loader from '../loader.reducer';
+import React from 'react';
+import {HIDE_LOADER, SHOW_LOADER} from '../../actions/index.actions';
+
+describe('Loader', () => {
+  const previousState = {};
+  it('check Show_Loader Reducer', () => {
+    const action = {type: SHOW_LOADER};
+    const result = loader(previousState, action);
+    expect(result).toEqual({isLoaderVisible: true});
+  });
+  it('check Hide_Loader Reducer', () => {
+    const action = {type: HIDE_LOADER};
+    const result = loader(previousState, action);
+    expect(result).toEqual({isLoaderVisible: false});
+  });
+  it('check Default Reducer', () => {
+    const action = {};
+    const result = loader(previousState, action);
+    expect(result).toEqual({isLoaderVisible: false});
+  });
+});
