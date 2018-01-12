4350c6a21cf470b81a6f69c9242b2ae6e61b3ce1
Wiwat Patanaprasitchai <wiwatpatanaprasitchai@Wiwats-MacBook-Pro.local>
2018-01-12 16:13:06 +0700

    sega

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,7 +1,10 @@
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/root.reducer';
 // import someReduxMiddleware from 'some-redux-middleware';
 // import someOtherReduxMiddleware from 'some-other-redux-middleware';
 import {applyMiddleware, compose, createStore/* , applyMiddleware*/} from 'redux';
+// import {call, fork, put, take, takeEvery} from 'redux-saga/effects';
+import {fork, put, takeEvery} from 'redux-saga/effects';
 
 const enhancerList = [];
 const devToolsExtension = window && window.__REDUX_DEVTOOLS_EXTENSION__;
@@ -18,7 +21,43 @@ const logger = () => (next) => ((action) => {
   next(action);
 });
 
-const composedEnhancer = compose(applyMiddleware(logger), ...enhancerList);
+const composeEnhancers = global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
-export const initStore = () => createStore(rootReducer, {}, composedEnhancer);
+const sagaMiddleware = createSagaMiddleware();
+
+const composedEnhancer = composeEnhancers(applyMiddleware(sagaMiddleware, logger));
+
+function* fetchHandler () {
+  yield put({
+    type: 'SHOW_LOADER'
+  });
+  yield put({
+    type: 'LOAD_SERVER',
+    payload: [{
+      title: 'title',
+      content: 'sega',
+      id: 1
+    }]
+  });
+  yield put({
+    type: 'HIDE_LOADER'
+  });
+  // console.log('SHOW_LOADER');
+}
+
+function* notes () {
+  yield takeEvery('FATCH_NOTE', fetchHandler);
+  // console.log('note saga');
+}
+
+function* sagas () {
+  yield fork(notes);
+  // console.log('index saga');
+}
+
+export const initStore = () => {
+  const store = createStore(rootReducer, {}, composedEnhancer);
+  sagaMiddleware.run(sagas);
+  return store;
+};
 
