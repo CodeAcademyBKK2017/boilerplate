ca4712900a0ebe3b3a2cced32b80858fb53e5be2
pheerah <pheerah.p@gmail.com>
2018-01-12 14:36:27 +0700

    test case

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,6 +1,3 @@
-import rootReducer from './reducers/root.reducer';
-import {applyMiddleware, compose, createStore} from 'redux';
-
 const enhancerList = [];
 const devToolsExtension = window && window.__REDUX_DEVTOOLS_EXTENSION__;
 
@@ -8,12 +5,3 @@ if (typeof devToolsExtension === 'function') {
   enhancerList.push(devToolsExtension());
 }
 
-const logger = ({dispatch, getState}) => (next) => (action) => {
-  if (action.type !== 'Navigation/NAVIGATE') {
-    next(action);
-  }
-};
-
-const composedEnhancer = compose(applyMiddleware(logger), ...enhancerList);
-
-export const initStore = () => createStore(rootReducer, {}, composedEnhancer);
