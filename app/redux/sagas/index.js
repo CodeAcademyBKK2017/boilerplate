8439f6e0c8b8f2ea5139e49a66ed81f59ed82893
Thanaphat Suwannikornkul <onsensei@gmail.com>
2018-01-15 11:17:39 +0700

    move saga to new file

---
+++ app/redux/sagas/index.js
@@ -0,0 +1,29 @@
+import {delay} from 'redux-saga';
+import {put, takeEvery} from 'redux-saga/effects';
+
+function* fetchNoteHandler () {
+  yield put({
+    type: 'SHOW_LOADER'
+  });
+  
+  yield delay(3000);
+  
+  yield put({
+    type: 'POPULATE_NOTES',
+    payload: [{
+      id: 0,
+      title: 'title from sage',
+      content: 'content from sage'
+    }]
+  });
+
+  yield put({
+    type: 'HIDE_LOADER'
+  });
+}
+
+function* notesSaga () {
+  yield takeEvery('FETCH_NOTES', fetchNoteHandler);
+}
+
+export default notesSaga;
--- app/redux/store.js
+++ app/redux/store.js
@@ -1,9 +1,10 @@
 import createSagaMiddleware from 'redux-saga';
 // import someReduxMiddleware from 'some-redux-middleware';
 // import someOtherReduxMiddleware from 'some-other-redux-middleware';
+import notesSage from './sagas/index';
 import rootReducer from './reducers/root.reducer';
 import {applyMiddleware, compose, createStore} from 'redux';
-import {fork, put, takeEvery} from 'redux-saga/effects';
+import {fork} from 'redux-saga/effects';
 
 const logger = () => (next) => (action) => {
   // console.log('action is', action);
@@ -16,31 +17,8 @@ const sagaMiddleware = createSagaMiddleware();
 
 const composedEnhancer = composeEnhancers(applyMiddleware(sagaMiddleware, logger));
 
-function* fetchNoteHandler () {
-  yield put({
-    type: 'SHOW_LOADER'
-  });
-  
-  yield put({
-    type: 'POPULATE_NOTES',
-    payload: [{
-      id: 0,
-      title: 'title from sage',
-      content: 'content from sage'
-    }]
-  });
-
-  yield put({
-    type: 'HIDE_LOADER'
-  });
-}
-
-function* notes () {
-  yield takeEvery('FETCH_NOTES', fetchNoteHandler);
-}
-
 function* sagas () {
-  yield fork(notes);
+  yield fork(notesSage);
 }
 
 export const initStore = () => {
