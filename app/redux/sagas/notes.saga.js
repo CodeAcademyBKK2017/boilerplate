1f46044c0ff74b02592fc5ee35cbabb1228a3dc7
deathsky0725 <assassin@Benjapols-iMac.local>
2018-01-16 11:51:14 +0700

    Update Change sagas to the notes.saga.js

---
+++ app/redux/sagas/notes.saga.js
@@ -0,0 +1,72 @@
+import ApiNotes from '../../api';
+import {Alert} from 'react-native';
+import {call, put,  select, take, takeLatest} from 'redux-saga/effects';
+import {delay} from 'redux-saga';
+import {filterNote} from '../../utils/transformerutil';
+import {getItemToStorage, setItemToStorage} from '../../utils/storageutil';
+import * as actions from '../actions/index.actions';
+
+function* fetchHandler () {
+  yield put(actions.showLoader());
+  yield delay(2000);
+  let response;
+  try {
+    response = yield call(ApiNotes.getNotes);
+  } catch (error) {
+    const storageData =  yield call(getItemToStorage, 'storageNote');
+    response = storageData ? storageData : [];
+  }
+  yield put(actions.populateNotes(response));
+  yield put(actions.hideLoader());
+}
+function* addNoteHandler (action) {
+  yield put(actions.showLoader());
+  try {
+    const note = action.payload;
+    const noteWithId = yield call(ApiNotes.addNote, note);
+    const currentNote = yield select((store) => (store.notes));
+    const newNote = [...currentNote, noteWithId];
+    yield put(actions.addNote(noteWithId)); 
+    yield call(setItemToStorage, 'storageNote', newNote);
+  } catch (error) {
+    Alert.alert(
+      'Save Failed', 
+      String(error),
+      [
+        {text: 'OK'}
+      ],
+      {
+        cancelable: false
+      }
+    );
+  }
+  yield put(actions.hideLoader());
+}
+function* deleteNoteHandler (action) {
+  yield put(actions.showLoader());
+  try {
+    const itemId = action.payload;
+    yield call(ApiNotes.deleteNote, itemId);
+    yield put(actions.deleteNote(itemId)); 
+    const currentNote = yield select((store) => (store.notes));
+    const remainNote = filterNote(currentNote, itemId);
+    yield call(setItemToStorage, 'storageNote', remainNote);
+  } catch (error) {
+    Alert.alert(
+      'Delete Failed',
+      String(error),
+      null,
+      {
+        cancelable: false
+      }
+    );
+  }
+  yield put(actions.hideLoader());
+}
+export default function* notes () {
+  yield take(actions.fetchNotes);
+  yield fetchHandler();
+
+  yield takeLatest(actions.ADD_NOTE_REQUEST, addNoteHandler);
+  yield takeLatest(actions.DELETE_NOTE_REQUEST, deleteNoteHandler);
+}
