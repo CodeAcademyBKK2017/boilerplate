d7c1a0411120c05da96fa0671b3e02a7ce1c0e50
Suphawut Pitirit <suphawut_pit@truecorp.co.th>
2018-03-07 14:25:56 +0700

    testjenkin2

--- app/util/utility.js
+++ app/util/utility.js
@@ -3,10 +3,10 @@ import {AsyncStorage} from 'react-native';
 class Utility {
     filterNotes = (notes, idToFilter) => notes.filter((item) => item.id !== idToFilter);
     setItemToStroage = (key, data) =>  AsyncStorage.setItem(key, JSON.stringify(data));
-    getItemToStroage = async () => {
+    getItemToStroage = async (key) => {
       try {
-        // const data = await AsyncStorage.getItem(key);
-        return 0; // JSON.parse(data);
+        const data = await AsyncStorage.getItem(key);
+        return JSON.parse(data);
       } catch (e) {
         return e;
       }
